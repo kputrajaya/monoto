9ad9ec87e1b1f74b31a57adcc584fc491cce44aa
kiloev <freakperzon@yahoo.com>
2020-07-03 19:08:04 +0700

    Revert auth domain

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDMLzj5TqoFIIDaFoG6Rr8aNfn-jaqiGMs',
+  appId: '1:646929563973:web:7f579ed804b6a392b2be2c',
   authDomain: 'auth.monoto.app',
   databaseURL: 'https://monote-app.firebaseio.com',
   projectId: 'monote-app',
   storageBucket: 'monote-app.appspot.com',
-  messagingSenderId: '646929563973',
-  appId: '1:646929563973:web:7f579ed804b6a392b2be2c',
-  measurementId: 'G-X9FSVCH70W'
+  measurementId: 'G-X9FSVCH70W',
+  messagingSenderId: '646929563973'
 };
 firebase.initializeApp(firebaseConfig);
 
